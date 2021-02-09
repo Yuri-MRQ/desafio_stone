0a216331359ac67eb5f5c770c2add626f689463a
Yuri Macedo Rezende Quintanilha <ymquint@gmail.com>
2021-02-09 19:42:27 -0300

    Add erro handling

--- desafio_stone.py
+++ desafio_stone.py
@@ -21,39 +21,41 @@ def split_bill(items_list, persons_list):
     except:
         items_list = [[]]
         pass
-    try:
-        if not items_list[0] or not persons_list:
-            if not items_list[0]:
-                print("Lista de itens vazia")
-            elif not persons_list:
-                print("Lista de e-mails vazia")
-        else:
-            array_items_list = np.array(items_list) #turn in a np.ndarray where each row is a different item
+
+    if not items_list[0] or not persons_list:
+        if not items_list[0]:
+            return "Lista de itens vazia"
+        elif not persons_list:
+            return "Lista de e-mails vazia"
+    else:
+        array_items_list = np.array(items_list) #turn in a np.ndarray where each row is a different item
+        try:
             values = array_items_list[:,1: ].astype(int) 
-            sum_items = np.sum(np.prod(values, axis=1)) #vectorazation
+        except ValueError as e:
+            return "Inserido float number, por favor verifique seu input"
 
-            L = len(persons_list) #how many e-mail exists
-            dict_valeu_per_person = {} #final answer
-            if sum_items%L == 0:
-                value_per_person = sum_items/L
+        sum_items = np.sum(np.prod(values, axis=1)) #vectorazation
+
+        L = len(persons_list) #how many e-mail exists
+        dict_valeu_per_person = {} #final answer
+        if sum_items%L == 0:
+            value_per_person = sum_items/L
+            for email in persons_list:
+                dict_valeu_per_person[email] = int(value_per_person)
+        else:
+            reminder = sum_items%L
+            reminder_ = reminder #to make sure I'm gonna split the reminder correctly
+            if (sum_items - reminder)%L == 0:
+                value_per_person = (sum_items - reminder)/L
+                shuffle(persons_list) #to be more fair,shuffle the list
                 for email in persons_list:
-                    dict_valeu_per_person[email] = int(value_per_person)
+                    dict_valeu_per_person[email] = int(value_per_person) + int(1 if reminder_ > 0 else 0)
+                    reminder_ -= 1
             else:
-                reminder = sum_items%L
-                reminder_ = reminder #to make sure I'm gonna split the reminder correctly
-                if (sum_items - reminder)%L == 0:
-                    value_per_person = (sum_items - reminder)/L
-                    shuffle(persons_list) #to be more fair,shuffle the list
-                    for email in persons_list:
-                        dict_valeu_per_person[email] = int(value_per_person) + int(1 if reminder_ > 0 else 0)
-                        reminder_ -= 1
-                else:
-                    print("(Soma do itens - resto) % quantidade de pessoas: {}".format((sum_items - reminder)%L))
+                return "(Soma do itens - resto) % quantidade de pessoas: {}".format((sum_items - reminder)%L)
 
-            return dict_valeu_per_person
+        return dict_valeu_per_person
 
-    except Exception as e:
-        print("Seguinte erro ocorreu:", e)
 
     
     
